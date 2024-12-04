b116660ec00cc0e6179e6b1a17f03d75462a1d10
maita07 <josemaitapitado@gmail.com>
2024-12-04 11:56:01 -0300

    Error de tes

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 app.get('/', (req, res) => {
-  res.status(200).send('Adios Mundo!');
+  res.status(200).send('Hola Mundo!');
 });
 
 describe('GET /', () => {
